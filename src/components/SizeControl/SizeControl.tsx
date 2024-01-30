8f4c26bcbc4a4aa7887c9a0bab3a3be5bd933045
PromiSe#### <vicimpa@ya.ru>
2024-01-30 08:07:24 +0000

    --wip-- [skip ci]

--- src/components/SizeControl/SizeControl.tsx
+++ src/components/SizeControl/SizeControl.tsx
@@ -1,19 +1,20 @@
-import s from "./SizeControl.module.sass"
-import {useNodeHud} from "@/components/NodeEditor";
-import {FullScreen} from "@/components/SizeControl/components/FullScreen";
-import {Zoom} from "@/components/SizeControl/components/Zoom";
-import {Center} from "@/components/SizeControl/components/Center";
+import { useNodeHud } from "@/components/NodeEditor";
+import { Center } from "@/components/SizeControl/components/Center";
+import { FullScreen } from "@/components/SizeControl/components/FullScreen";
+import { Zoom } from "@/components/SizeControl/components/Zoom";
+
+import s from "./SizeControl.module.sass";
 
 export const SizeControl = () => {
-    const hud = useNodeHud();
+  const hud = useNodeHud();
 
-    return (
-        <hud.Portal>
-            <div className={s.control}>
-                <Zoom/>
-                <FullScreen />
-                <Center/>
-            </div>
-        </hud.Portal>
-    );
+  return (
+    <hud.Portal>
+      <div className={s.control}>
+        <Zoom />
+        <FullScreen />
+        <Center />
+      </div>
+    </hud.Portal>
+  );
 };
