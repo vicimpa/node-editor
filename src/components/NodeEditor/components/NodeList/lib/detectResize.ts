6f47b6553c3a08f3d9d52289fd18357eaabedc85
PromiSe#### <vicimpa@ya.ru>
2024-01-30 13:03:49 +0000

    Fix detect resizer

--- src/components/NodeEditor/components/NodeList/lib/detectResize.ts
+++ src/components/NodeEditor/components/NodeList/lib/detectResize.ts
@@ -1,19 +1,29 @@
-import { resizer } from "@/utils/resizer";
-import { batch, effect } from "@preact/signals-react";
+import { subscribe } from "@/utils/reactive";
+import { batch, effect, signal } from "@preact/signals-react";
 
 import { NodeListCtx } from "../";
 
 export const detectResize = (list: NodeListCtx) => (
-  effect(() => (
-    list.ref.current &&
-    resizer(list.ref.current, () => (
+  effect(() => {
+    const size = signal(0);
+    const dis = subscribe(list.list, () => size.value = list.list.size);
+
+    for (const [, value] of list.list) {
+      value.x.value;
+      value.y.value;
+    }
+
+    if (list.ref.current)
       batch(() => {
         const bbox = list.ref.current?.getBBox()!;
         list.x.value = bbox.x ?? 0;
         list.y.value = bbox.y ?? 0;
         list.width.value = bbox.width ?? 0;
         list.height.value = bbox.height ?? 0;
-      })
-    ))
-  ))
+      });
+
+    return () => {
+      dis();
+    };
+  })
 );
