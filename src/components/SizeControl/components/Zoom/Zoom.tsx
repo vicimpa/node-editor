880ef7c9c73dac1573ced8fa50dd6023d414645d
PromiSe#### <vicimpa@ya.ru>
2024-01-30 08:22:50 +0000

    --wip-- [skip ci]

--- src/components/SizeControl/components/Zoom/Zoom.tsx
+++ src/components/SizeControl/components/Zoom/Zoom.tsx
@@ -1,21 +1,34 @@
 import { useNodeMap } from "@/components/NodeEditor";
 import s from "@/components/SizeControl/SizeControl.module.sass";
+import { useWindowEvent } from "@/hooks/useWindowEvent";
+import { looper } from "@/utils/looper";
+import { useSignal, useSignalEffect } from "@preact/signals-react";
 
 const DELTA_ZOOM = .12;
+
 export const Zoom = () => {
   const map = useNodeMap();
+  const delta = useSignal(0);
 
-  const zoomIn = () => {
-    map.toScale(v => v + DELTA_ZOOM);
-  };
-  const zoomOut = () => {
-    map.toScale(v => v - DELTA_ZOOM);
-  };
+  useWindowEvent(['mouseup', 'blur'], () => {
+    delta.value = 0;
+  });
+
+  useSignalEffect(() => {
+    if (!delta.value)
+      return;
+
+    return looper((_, dtime) => {
+      map.toScale((v) => (
+        v + delta.peek() * dtime * DELTA_ZOOM * .01
+      ));
+    });
+  });
 
   return (
     <>
-      <div className={s.item} onClick={zoomIn}>+</div>
-      <div className={s.item} onClick={zoomOut}>-</div>
+      <div className={s.item} onMouseDown={() => delta.value += 1}>+</div>
+      <div className={s.item} onMouseDown={() => delta.value -= 1}>-</div>
     </>
   );
 };
