9c329cfdecbbe83203b888525cc2b0eb905e759a
PromiSe#### <vicimpa@ya.ru>
2024-01-31 06:25:11 +0000

    Append iters

--- src/utils/domrect.ts
+++ src/utils/domrect.ts
@@ -1,4 +1,6 @@
-import {Vec2} from "@/library/Vec2";
+import { Vec2 } from "@/library/Vec2";
+
+import { minMax } from "./math";
 
 export const rectToString = (rect: DOMRect) => (
   `${rect.x} ${rect.y} ${rect.width} ${rect.height}`
@@ -10,3 +12,13 @@ export const rectCenter = (rect: DOMRect | HTMLElement): Vec2 => {
 
   return Vec2.fromSize(rect).div(2).plus(rect);
 };
+
+export const rectIters = (a: DOMRect, b: DOMRect, i: number): DOMRect => (
+  i = minMax(i, 0, 1),
+  new DOMRect(
+    a.x + (b.x - a.x) * i,
+    a.y + (b.y - a.y) * i,
+    a.width + (b.width - a.width) * i,
+    a.height + (b.height - a.height) * i,
+  )
+);
