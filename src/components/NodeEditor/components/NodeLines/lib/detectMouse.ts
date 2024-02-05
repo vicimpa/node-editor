50f1bb9654f920f4dad3b1adee1f702260bd931f
Sergey Ivanov <max3xxx3@gmail.com>
2024-02-05 13:05:59 +0300

    --wip-- [skip ci]

--- src/components/NodeEditor/components/NodeLines/lib/detectMouse.ts
+++ src/components/NodeEditor/components/NodeLines/lib/detectMouse.ts
@@ -3,8 +3,7 @@ import {windowEvent} from "@/utils/events";
 import {effect} from "@preact/signals-react";
 
 import {NodeLinesCtx} from "../";
-import {looper} from "@/utils/looper.ts";
-import {makeDrag} from "@/utils/makeDrag.ts";
+
 
 export const detectMouse = (lines: NodeLinesCtx) => (
   effect(() => {
@@ -12,21 +11,8 @@ export const detectMouse = (lines: NodeLinesCtx) => (
     if (!active) return;
     const {map} = lines
 
-    const moveView = makeDrag(({current}) => {
-      const dispose = looper((_, dtime) => {
-        map.calcViewTransitionVec(current, dtime).toSignals(map.x, map.y)
-      })
-      return ({current: newCurrent}) => {
-        current.set(newCurrent);
-        return dispose
-      };
-    })
-
-    return windowEvent(['mousedown', 'mousemove'], (e) => {
-      if (e.type === "mousemove")
-        lines.mouse.value = map.offset(Vec2.fromPageXY(e))
-      else
-        return moveView(e)
+    return windowEvent('mousemove', (e) => {
+      lines.mouse.value = map.offset(Vec2.fromPageXY(e))
     });
   })
 );
--- src/components/NodeEditor/components/NodePort/lib/detectConnect.ts
+++ src/components/NodeEditor/components/NodePort/lib/detectConnect.ts
@@ -1,19 +1,35 @@
-import { dispose } from "@/utils/dispose";
-import { refEvent } from "@/utils/events";
-import { effect } from "@preact/signals-react";
+import {dispose} from "@/utils/dispose";
+import {refEvent} from "@/utils/events";
+import {effect} from "@preact/signals-react";
 
-import { NodePortCtx } from "../";
+import {NodePortCtx} from "../";
+import {makeDrag} from "@/utils/makeDrag.ts";
+import {looper} from "@/utils/looper.ts";
 
 export const detectConnect = (port: NodePortCtx) => (
   effect(() => {
     if (!port.ref.current)
       return;
+    const {lines} = port
+    const {map} = lines
+
+    const moveView = makeDrag(({current}) => {
+      const dispose = looper((_, dtime) => {
+        map.calcViewTransitionVec(current, dtime).toSignals(map.x, map.y)
+        lines.mouse.value = map.offset(current);
+      })
+      return ({current: newCurrent}) => {
+        current.set(newCurrent);
+        return dispose
+      };
+    })
 
     return dispose([
       refEvent(port.ref, 'mousedown', (e) => {
         e.stopPropagation();
         e.preventDefault();
         port.lines.from(port);
+        return moveView(e)
       }),
       refEvent(port.ref, 'mouseup', () => {
         port.lines.to(port);
